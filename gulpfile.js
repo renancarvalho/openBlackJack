1c7c94277a0abcd09a6bbdfbb53fed4366c7c7b4
Renan Carvalho <renan.carvalho@getchute.com>
2015-06-20 13:54:08 -0300

    fixed bug in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('less', function () {
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('style/'));
+    .pipe(gulp.dest('front/style/'));
 });
 
 gulp.task('webpack', function () {
